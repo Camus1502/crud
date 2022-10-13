cbf85e94f535925354da7d1f6be48dbcd0857ae7
Camus1502 <diego.camusfrez54@gmail.com>
2022-10-12 23:14:49 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {useState} from 'react';
 
-import { isEmpty } from 'lodash';
+import { isEmpty, size } from 'lodash';
 import shortid from 'shortid';
 
 export function App(){
   const [tarea, setTarea] = useState("")
   const [tareas, setTareas] = useState([])
+  const [editTareas, setEditTareas] = useState(false)
+  const [id, setId] = useState()
 
   const addTarea = (event) => {
     event.preventDefault();
@@ -21,6 +23,31 @@ export function App(){
     setTareas([...tareas, newTarea])
       setTarea("")
   }
+
+  const deleteTarea = (id) => {
+    const filterTareas = tareas.filter(tarea => tarea.id !== id)
+    setTareas(filterTareas)
+  }
+
+  const editTarea = (theTarea) => {
+    setTarea(theTarea.name)
+    setEditTareas(true)
+    setId(theTarea.id)
+  }
+
+  const guardarTarea = (event) => {
+    event.preventDefault();
+    if (isEmpty(tarea)){
+      console.log('Tarea Vacia')
+      return
+    }
+    
+    const editedTarea = tareas.map(item => item.id === id ? {id, name: tarea} : item)
+    setTareas(editedTarea)
+    setEditTareas(false)
+    setTarea("")
+    setId("")
+  }
   return (
     <div className="container mt-5">
         <h1 className="text-center">Tareas</h1>
@@ -28,22 +55,37 @@ export function App(){
         <div className="row">
           <div className="col-6">
             <h4 className="text-center">Lista de Tareas</h4>
-            <ul className="list-group">
-              {
-              tareas.map((tarea) => (
-                  <li className="list-group-item" key={tarea.id}>
-                    <span className="lead">{tarea.name}</span>
-                    <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
-                    <button className="btn btn-warning btn-sm float-right">Editar</button>
-                  </li>
-              ))
-              
-              }    
-            </ul>
+            {
+              size(tareas) == 0 ? (
+                <h5 className="text-center">No hay Tareas </h5>
+              ) : (
+
+                <ul className="list-group">
+                  {
+                    tareas.map((tarea) => (
+                      <li className="list-group-item" key={tarea.id}>
+                        <span className="lead">{tarea.name}</span>
+                        <button 
+                          className="btn btn-danger btn-sm float-right mx-2"
+                          onClick={() => deleteTarea(tarea.id)}>
+                            Eliminar
+                        </button>
+
+                        <button 
+                          className="btn btn-warning btn-sm float-right"
+                          onClick={() => editTarea(tarea)}>
+                            Editar
+                        </button>
+                      </li>
+                    ))
+                  } 
+                </ul>
+              )  
+            }
           </div>
           <div className="col-6">
-            <h4 className="text-center">Formulario</h4>
-            <form onSubmit={(addTarea) }>
+            <h4 className="text-center">{editTareas ? "Modificar" : "Guardar"}</h4>
+            <form onSubmit={ editTareas ? guardarTarea : addTarea }>
               <input 
                 type="text" 
                 className="form-control mb-2" 
@@ -51,7 +93,11 @@ export function App(){
                 onChange={(text) => setTarea(text.target.value)}
                 value={tarea}
               />
-              <button className="btn btn-dark btn-block" type="submit">Agregar</button>
+              <button 
+                className={editTareas ? "btn btn-warning btn-block": "btn btn-dark btn-block"} 
+                type="submit">
+                  {editTareas ? "Guardar" : "Agregar"}
+              </button>
             </form>
           </div>
         </div>
